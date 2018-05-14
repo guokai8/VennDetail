05d82af6a168bd1cdf1eef1880065dbb81a1c278
bioguo <guokai8@gmail.com>
2018-05-14 14:10:58 -0500

    get Features based on venn results
    get Features based on venn results combine with input

---
+++ R/getFeature.R
@@ -0,0 +1,46 @@
+##' @name getFeature
+##' @title get feature based on venn results
+##' @rdname getFeature-methods
+##' @importFrom dplyr filter
+##' @importFrom dplyr left_join
+##' @importFrom dplyr full_join
+##' @importFrom dplyr right_join
+##' @importFrom tibble rownames_to_column
+##' @importFrom magrittr %>%
+##' @importFrom purrr reduce
+##' @param object venn object
+##' @param group group you want used
+##' @param rlist detail information you want extract
+##' @param sep separate for new colnames
+##' @export
+##' @examples
+##' \dontrun{
+##' A <- sample(1:100, 40, replace = FALSE);
+##' B <- sample(1:100, 60, replace = FALSE);
+##' C <- sample(1:100, 40, replace = FALSE);
+##' dA=data.frame(A=A,"FC"=rnorm(40))
+##' dB=data.frame(B=B,"FC"=rnorm(60))
+##' dC=data.frame(C=C,"FC"=rnorm(40))
+##' res<-venndetail(list(A=A,B=B,C=C),plot=TRUE)
+##' getFeature(res,group="Shared",rlist=list(dA,dB,dC))
+##' }
+setMethod("getFeature",signature = (object="venn"),function(object,group,rlist,userowname=TRUE,gind=NULL,sep="_",...){
+  dd<-object@result
+  lhs<-dd%>%filter(Group%in%group)
+  lhs$Detail<-as.character(lhs$Detail)
+  if(is.null(names(rlist))){
+    names(rlist)<-paste("Group",1:length(rlist),sep="")
+  }
+  name<-names(rlist)
+  rlist<-Map(function(x,y).pasten(x,y,sep=sep),rlist,name)
+  if(userowname==FALSE){
+    if(is.null(gind)){
+      gind=rep(1,length(rlist))
+    }
+    rlist=Map(function(x,y).setrownames(x,y),rlist,gind)
+  }
+  rlist<-lapply(rlist, function(x).add_colnames(x))
+  rr<-Reduce(function(x,y)rowjoin(x,y,fun="full_join"),rlist)
+  rhs<-left_join(lhs,rr,by=c("Detail"="rown"))
+  return(rhs)
+})
