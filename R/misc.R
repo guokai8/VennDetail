1d3260eb625fafd5356a482149c140071b5f5f6f
bioguo <guokai8@gmail.com>
2018-05-14 14:08:38 -0500

    Update misc.R

--- R/misc.R
+++ R/misc.R
@@ -35,5 +35,41 @@ result.venn<-function(x){
 detail.venn<-function(x){
   x@detail
 }
+##' @importFrom magrittr %>%
+##' @importFrom dplyr select
+##' @importFrom dplyr everything
+.add_colnames<-function(x){
+  if(!"rown"%in%colnames(x)){
+    x$rown<-rownames(x)
+  }
+  x%>%select(rown,everything())
+}
+
+.pasten<-function(x,name,sep="_"){
+  colnames(x)<-paste(name,colnames(x),sep=sep)
+  return(x)
+}
+.setrownames<-function(x,gind){
+  rownames(x)=as.character(x[,gind])
+  return(x[,-gind,drop=F])
+}
+##' @method rowname join
+##' @title join by rownames
+##' @rdname rowjoin-methods
+##' @importFrom dplyr full_join
+##' @importFrom dplyr left_join
+##' @importFrom dplyr right_join
+##' @importFrom dplyr anti_join
+##' @importFrom dplyr semi_join
+##' @param x data.frame x
+##' @param y data.frame y
+##' @param fun join type
+##' @export
+setMethod("rowjoin",signature(x="data.frame",y="data.frame"),function(x,y,fun="full_join"){
+  x<-.add_colnames(x)
+  y<-.add_colnames(y)
+  f=match.fun(fun)
+  return(f(x,y,by=c("rown"="rown")))
+})
 
 
