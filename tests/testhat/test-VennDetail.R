973ab856c496e103387984eacfbdadd094b71b01
bioguo <guokai8@gmail.com>
2019-02-07 13:19:20 -0600

    Delete test-VennDetail.R

--- tests/testhat/test-VennDetail.R
+++
@@ -1,6 +0,0 @@
-options(device=pdf())
-A <- sample(1:100, 40, replace = FALSE);
-B <- sample(1:100, 60, replace = FALSE);
-C <- sample(1:100, 40, replace = FALSE);
-expect_error(venndetail(list(A=A,B=B,C=C),plot=TRUE))
-dev.off()
