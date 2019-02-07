366e68297a44e6b3293ffccd5223847131436f08
bioguo <guokai8@gmail.com>
2019-02-07 13:19:26 -0600

    Delete test-vennpie.R

--- tests/testhat/test-vennpie.R
+++
@@ -1,7 +0,0 @@
-options(device=pdf())
-A <- sample(1:100, 40, replace = FALSE);
-B <- sample(1:100, 60, replace = FALSE);
-C <- sample(1:100, 40, replace = FALSE);
-res<-venndetail(list(A=A,B=B,C=C),plot=TRUE)
-expect_error(vennpie(res))
-dev.off()
