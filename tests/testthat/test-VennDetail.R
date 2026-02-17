0fe8e033928598b02f0560f818ccb7fdf8321ae3
Kai Guo <guokai8@gmail.com>
2026-02-17 10:42:18 +0800

    Add files via upload

---
+++ tests/testthat/test-VennDetail.R
@@ -0,0 +1,6 @@
+library(VennDetail)
+context("venndetail")
+A <- sample(1:100, 40, replace = FALSE)
+B <- sample(1:100, 60, replace = FALSE)
+C <- sample(1:100, 40, replace = FALSE)
+expect_silent(res<-venndetail(list(A = A,B = B,C = C)))
